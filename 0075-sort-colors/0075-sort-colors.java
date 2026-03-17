4fda98dbc71ee65c557009f16b3325272d404b86
Shubham Singh <shubham.singh_cs23@gla.ac.in>
2026-03-17 19:07:47 +0530

    Time: 0 ms (100%), Space: 43.5 MB (74.79%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int start=0;
+        int mid=0;
+        int end=nums.length-1;
+        while(mid<=end){
+            if(nums[mid]==0){
+                int temp=nums[mid];
+                nums[mid]=nums[start];
+                nums[start]=temp;
+                start++;
+                mid++;
+            }
+            else if(nums[mid]==1) mid++;
+            else{
+                int temp=nums[mid];
+                nums[mid]=nums[end];
+                nums[end]=temp;
+                end--;
+
+            }
+        
+    }
+    
+    
+}
+}
